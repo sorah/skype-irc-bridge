140e6de43e28ede0a6c5519075c23f0514bc1df4
Shota Fukumori (sora_h) <her@sorah.jp>
2013-10-19 04:02:51 +0900

    Handle message editing

--- skype_irc_bridge.py
+++ skype_irc_bridge.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim: noet sts=4:ts=4:sw=4
 # author: takano32 <tak at no32.tk>
-#
+# modified-by: sorah <her at sorah.jp>
 
 import Skype4Py
 import os
@@ -25,18 +25,21 @@ class SkypeIrcBridge():
 		if len(msg.Body) == 0:
 			return
 		if event == u"RECEIVED":
+			self._pass_to_irc(msg)
+
+	def _pass_to_irc(self, msg):
 			said = False
 			for key in CONFIG:
 				if key == 'skype' or key == 'irc':
 					continue
 				if CONFIG[key].has_key('skype') and msg.ChatName == CONFIG[key]['skype'] and CONFIG[key].has_key('irc'):
 					said = True
-					self._pass_to_irc(CONFIG[key]['irc'], msg)
+					self._pass_to_irc_core(CONFIG[key]['irc'], msg)
 			if not said:
-				self._pass_to_irc(False, msg)
+				self._pass_to_irc_core(False, msg)
 
 	@staticmethod
-	def _pass_to_irc(channel, msg):
+	def _pass_to_irc_core(channel, msg):
 		name = msg.Sender.FullName
 		if len(name) == 0 or len(name) > 16:
 			name = msg.Sender.Handle
@@ -50,6 +53,12 @@ class SkypeIrcBridge():
 		for line in msg.Body.splitlines():
 			if msg.Type != 'SAID':
 				name = '[%s] %s' % (msg.Type, name)
+
+			if msg.EditedBy:
+				if msg.EditedBy == msg.Sender.Handle:
+					name = '[EDITED] ' + name
+				else:
+					name = '[EDITED BY %s] %s' % (msg.EditedBy, name)
 
 			if channel:
 				text = '%s: %s' % (name, line)
@@ -69,6 +78,11 @@ class SkypeIrcBridge():
 			else:
 				time.sleep(len(text) / 20.0)
 
+	def handler_notify(self, notification):
+		payload = notification.split(' ')
+		if payload[0] == 'CHATMESSAGE' and payload[2] == 'BODY':
+			self._pass_to_irc(self.skype.Message(payload[1]))
+
 	def say(self, channel, msg):
 		room = self.skype.Chat(channel)
 		print "Skype(%s)<-IRC: %s" % (channel, msg)
@@ -86,6 +100,7 @@ class SkypeIrcBridge():
 
 	def start(self):
 		self.skype.OnMessageStatus = self.handler
+		self.skype.OnNotify = self.handler_notify
 		self.skype.Attach()
 
 if __name__ == "__main__":
